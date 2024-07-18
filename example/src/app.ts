700a5af388183ff4e25866e5b3a11930f6093c59
Harry Xiong <harryxiong24@gmail.com>
2024-07-17 21:36:44 -0700

    feat: update packages

--- example/src/app.ts
+++ example/src/app.ts
@@ -1,4 +1,4 @@
-import { Application } from '../../src/main';
+import { Application } from 'koa-decorator-x';
 // import ErrorHandlerFunction from './middlewares/error-handler-function';
 
 // const app = new Application({
